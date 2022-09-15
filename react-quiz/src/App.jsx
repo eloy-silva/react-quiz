b200c8ab1974fbfb911893e97f63d9a8514a484c
Eloy Silva <weslley.eloy.silva@gmail.com>
2022-09-14 21:07:06 -0300

    Iniciando projeto com dependencias...

---
+++ react-quiz/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Fragment } from 'react'
+import './App.js'
+import GlobalStyle from  './styles/globalStyles.js'
+
+function App() {
+
+
+  return (
+    <Fragment>
+    <GlobalStyle/>
+    <h1>Quiz App</h1>
+ 
+    </Fragment>
+  )
+}
+
+export default App
